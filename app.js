1520d1b30e07ffe6653666cb47c976f7632b2142
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-28 16:18:29 +0200

    [UPD] FOTO POSTS CARICATE

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.static('public'));
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    console.log("Server del mio blog")
+    res.send("Server del mio blog")
+
 });
 
 app.listen(port, () => {
